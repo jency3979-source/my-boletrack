e8c049435f06873663e8e2e46848a6b6bbbf8b90
Jency Mary <jency3979@gmail.com>
2026-05-31 19:00:43 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDBj3CJBdhF06ro2DqR3VX38lOiQUq_KLA",
+    authDomain: "boletracker.firebaseapp.com",
+    projectId: "boletracker",
+    storageBucket: "boletracker.firebasestorage.app",
+    messagingSenderId: "752724700641",
+    appId: "1:752724700641:web:fee5f0bfcf311f67a9b6a0",
+    measurementId: "G-RKCTDB6T69"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+window.auth = auth;
+
+console.log("Firebase Loaded ✅");
